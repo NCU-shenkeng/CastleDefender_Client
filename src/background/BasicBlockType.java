74642cc66dfb4a1dae23fffc4e8c7d2715cda4a0
luko <luko8426@gmail.com>
2016-12-24 12:54:20 +0800

    Add background castle picture.

--- src/background/BasicBlockType.java
+++ src/background/BasicBlockType.java
@@ -11,7 +11,15 @@ public class BasicBlockType
 	public static final int FLOOR1 = 6;
 	public static final int FLOOR2 = 7;
 	public static final int DRAIN = 8;
-	public static final int CASTLE = 9;	
+	public static final int CASTLE1 = 9;
+	public static final int CASTLE2 = 10;
+	public static final int CASTLE3 = 11;
+	public static final int CASTLE4 = 12;
+	public static final int CASTLE5 = 13;
+	public static final int CASTLE6 = 14;
+	public static final int CASTLE7 = 15;
+	public static final int CASTLE8 = 16;
+	public static final int CASTLE9 = 17;
 	
-	public static final int NUMOFBLOCK = 10;	
+	public static final int NUMOFBLOCK = 18;	
 }
--- src/graphics/BackgroundGraphics.java
+++ src/graphics/BackgroundGraphics.java
@@ -35,7 +35,15 @@ public class BackgroundGraphics extends Graph
 			 					   "images/backgound/FLOOR1.bmp",
 			 					   "images/backgound/FLOOR2.bmp",
 			 					   "images/backgound/DRAIN.bmp",
-			 					   "images/backgound/CASTLE.bmp"};
+			 					   "images/backgound/CASTLE1.bmp",
+			 					   "images/backgound/CASTLE2.bmp",
+			 					   "images/backgound/CASTLE3.bmp",
+			 					   "images/backgound/CASTLE4.bmp",
+			 					   "images/backgound/CASTLE5.bmp",
+			 					   "images/backgound/CASTLE6.bmp",
+			 					   "images/backgound/CASTLE7.bmp",
+			 					   "images/backgound/CASTLE8.bmp",
+			 					   "images/backgound/CASTLE9.bmp"};
 	
 	int userViewWidth = 1100;
 	int userViewHigh = 700;
@@ -95,7 +103,10 @@ public class BackgroundGraphics extends Graph
 		{
 		  for (int j = BlockY - blockhighOffset; j <= BlockY + blockhighOffset; j++)
 		  {
-			  g.drawImage(img[map.scene[i][j].type], userviewX, userviewY, 100, 100, null);
+			  if (map.scene[i][j].type >= BasicBlockType.CASTLE1)
+				  g.drawImage(img[map.scene[i][j].type], userviewX, userviewY, 100, 100, null);
+			  else
+				  g.drawImage(img[map.scene[i][j].type], userviewX, userviewY, 100, 100, null);
 			  
 			  userviewY = userviewY + blockSize;
 		  }
@@ -150,7 +161,7 @@ public class BackgroundGraphics extends Graph
 	private void InitLocation()
 	{
 		this.BlockX = 4 + 9;
-		this.BlockY = 3 + 46;		
+		this.BlockY = 3 + 45;		
 	}
 	
 	private void OpenImge (BufferedImage img[])
